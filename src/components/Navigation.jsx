e1a1296c5ced48d4a15abc4c14ddf7f40f4511d0
MbaliPuso <123210276+MbaliPuso@users.noreply.github.com>
2024-07-11 01:33:38 +0200

    navigation component update

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
-import "../styles.css";
 
 const Navbar = () => {
-  return (
-    <>
-    <nav className="navbar navbar-expand-lg navbar-transparent bg-transparent navigation">
-      <a className="navbar-brand logo" href='/'>
-         {/* <img src={logo} alt="Drive logo" height="30" /> */}
-      </a>
-      <button className='navbar-toggler' type='button' data-toggle="collapse" data-target="#navbarNav" aria-controls='navbarNav' aria-expanded="false" aria-label='Toggle navigation'>
-        <span className='navbar-toggler-icon'></span>
-      </button>
-      <button className="order-now-button btn btn-danger">Order Now</button>
-      <div className='collapse navbar-collapse' id='navbarNav'>
-      <ul className='navbar-nav ml-auto'>
-        <li className='nav-item'><Link to="/">Home</Link></li>
-        <li className='nav-item'><Link to="/about">About Us</Link></li>
-        <li className='nav-item'><Link to="/menu">Menu</Link></li>
-      </ul>
+    return (
+        <>
+        {/* Navbar Start */}
+  <nav className="navbar navbar-expand-lg navbar-dark fixed-top py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
+    <a href="index.html" className="navbar-brand ms-4 ms-lg-0">
+      <h1 className="text-primary m-0">Baker</h1>
+    </a>
+    <button type="button" className="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarCollapse">
+      <div className="navbar-nav mx-auto p-4 p-lg-0">
+        <li className="nav-item nav-link active"><Link to='/'>Home</Link></li>
+        <li className="nav-item nav-link"><Link to='/about'>About</Link></li>
+        <li className="nav-item nav-link"><Link to='/menu'>Products</Link></li>
       </div>
-    </nav>
-    </>
-  );
+      <div className=" d-none d-lg-flex">
+        <div className="flex-shrink-0 btn-lg-square border border-light rounded-circle">
+          <i className="fa fa-phone text-primary" />
+        </div>
+        <div className="ps-3">
+          <small className="text-primary mb-0">Call Us</small>
+          <p className="text-light fs-5 mb-0">+012 345 6789</p>
+        </div>
+      </div>
+    </div>
+  </nav>
+  {/* Navbar End */}
+        </>
+    );
 };
 
 export default Navbar;
-
-
-
-
-
-
-
-
